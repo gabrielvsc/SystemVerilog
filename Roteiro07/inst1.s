a6a45ed7928a5334980520dc2ac431bcb3c58bb9
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2022-12-20 19:33:31 -0300

    refactor: refactor main

--- Roteiro07/inst1.s
+++ Roteiro07/inst1.s
@@ -1,14 +1,13 @@
 .text
 main:
-    addi a0, zero, 16
-    addi a1, zero, 1
-    addi s0, a0, 0
+    addi s0, zero, 5  # s0 = N <- Aqui é onde você insere o N
+    addi a0, zero, 1  # a0 = 1
 fact:
-    addi s3, zero, 1 
+    addi a1, zero, 1  # s3 = 1
 	loop:
-    	beq s0, a1,fim
-    	mul s3,s3,s0
-    	addi s0,s0,-1
+    	beq s0, a0,fim # if(s0 == a0) fim
+    	mul a1,a1,s0   # s3 = s3 * s0
+    	addi s0,s0,-1  # s0 = s0 - 1
     	jal zero, loop
 fim:
     nop
