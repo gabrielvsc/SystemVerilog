b11597513923fdea7c17dc6c9d916b13eacce8e9
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2022-12-27 17:07:47 -0300

    Roteiro 08 Código

---
+++ Roteiro08/inst1.s
@@ -0,0 +1,8 @@
+.text
+main:
+        addi x10, x0, 4
+        addi x11, x0, 3
+        addi x12, x0, 7
+        addi x13, x0, 5
+        addi x14, x0, 6
+        add x15, x10, x11
---
+++ Roteiro08/inst2.s
@@ -0,0 +1,8 @@
+.text
+main:
+        addi x10, x0, 4
+        add x11, x0, x10
+        addi x12, x0, 7
+        addi x13, x0, 5
+        addi x14, x0, 6
+        add x15, x10, x11
